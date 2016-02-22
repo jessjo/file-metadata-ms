7bf441ba4f246c1f1c4059c77684b806bd6d1a62
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-02-22 22:33:05 +0000

    upload file successful

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,20 +2,31 @@
 
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
+var express = require('express');
 
 module.exports = function (app) {
+var bodyParser = require('body-parser')
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+//app.use(express.json());     
+//app.use(express.urlencoded());
+
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/public/index.html');
-});
-
-app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
-	console.log(req.body); //form fields
-	/* example output:
-	{ title: 'abc' }
-	 */
-	console.log(req.file); //form files
-	/* example output:
+  })
+  
+app.post('/', upload.single('upl'), function(req,res){
+	  console.log("code reached")
+	  console.log(req.body); //form fields
+  	/* example output:
+  	{ title: 'abc' }
+  	 */
+  	console.log(req.file); //form files
+  	/* example output:
             { fieldname: 'upl',
               originalname: 'grumpy.png',
               encoding: '7bit',
@@ -25,8 +36,8 @@ app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
               path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
               size: 277056 }
 	 */
-	res.status(204).end();
-});
+	  res.status(204).end();
+  });
 
 
 };
