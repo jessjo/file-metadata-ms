ecebf3aa320d3600cc53d6f00f3763b111994840
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-02-22 19:13:47 +0000

    handling form field

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,25 @@ app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/public/index.html');
 });
-//Base case user visits home screen
+
+app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+	console.log(req.body); //form fields
+	/* example output:
+	{ title: 'abc' }
+	 */
+	console.log(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.status(204).end();
+});
+
 
 };
